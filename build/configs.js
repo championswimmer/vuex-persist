a7e8bcf9295effb350aa8b1ab4a1020f4f26a84e
Arnav Gupta <arnav@codingblocks.com>
2018-09-12 12:24:01 +0530

    separate packages for browser(umd), node (cjs) and bundlers (esm)
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ build/configs.js
@@ -0,0 +1,32 @@
+import {uglify} from 'rollup-plugin-uglify'
+
+export default {
+  umd: {
+    output: 'dist/umd/index.js',
+    format: 'umd',
+    target: 'es5',
+    globals: {'lodash.merge': '_.merge'},
+    env: 'development'
+  },
+  umdMin: {
+    output: 'dist/umd/index.min.js',
+    format: 'umd',
+    target: 'es5',
+    globals: {'lodash.merge': '_.merge'},
+    plugins: {
+      post: [uglify()]
+    },
+    env: 'production'
+  },
+  esm: {
+    output: 'dist/esm/index.js',
+    format: 'esm',
+    target: 'es2015',
+    genDts: true
+  },
+  cjs: {
+    output: 'dist/cjs/index.js',
+    format: 'cjs',
+    target: 'es2015',
+  }
+}
---
+++ build/rollup.config.js
@@ -0,0 +1,55 @@
+import replace from 'rollup-plugin-replace'
+import typescript from 'rollup-plugin-typescript2'
+import configs from './configs'
+
+const externals = [
+  'lodash.merge',
+  'vuex',
+  'circular-json'
+]
+
+const genTsPlugin = (configOpts) => typescript({
+  useTsconfigDeclarationDir: true,
+  tsconfigOverride: {
+    compilerOptions: {
+      target: configOpts.target,
+      declaration: configOpts.genDts
+    }
+  }
+})
+
+const genPlugins = (configOpts) => {
+  const plugins = []
+  if (configOpts.env) {
+    plugins.push(replace({
+      'process.env.NODE_ENV': JSON.stringify(configOpts.env)
+    }))
+  }
+  if (configOpts.plugins && configOpts.plugins.pre) {
+    plugins.push(...configOpts.plugins.pre)
+  }
+  plugins.push(genTsPlugin(configOpts))
+
+  if (configOpts.plugins && configOpts.plugins.post) {
+    plugins.push(...configOpts.plugins.post)
+  }
+  return plugins
+}
+
+const genConfig = (configOpts) => ({
+  input: 'src/index.ts',
+  output: {
+    file: configOpts.output,
+    format: configOpts.format,
+    name: 'VuexPersistence',
+    sourcemap: true,
+    exports: 'named',
+    globals: configOpts.globals,
+  },
+  external: externals,
+  plugins: genPlugins(configOpts)
+})
+
+const genAllConfigs = (configs) => (Object.keys(configs).map(key => genConfig(configs[key])))
+
+export default genAllConfigs(configs)
