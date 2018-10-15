1736d8d4d73c2edcaa897e7adc3ae100dd7dcc1c
hiendv <hien.dv.neo@gmail.com>
2018-10-15 17:11:26 +0700

    transpile esm and cjs formats in es5

--- build/configs.js
+++ build/configs.js
@@ -21,12 +21,12 @@ export default {
   esm: {
     output: 'dist/esm/index.js',
     format: 'esm',
-    target: 'es2015',
+    target: 'es5',
     genDts: true
   },
   cjs: {
     output: 'dist/cjs/index.js',
     format: 'cjs',
-    target: 'es2015',
+    target: 'es5'
   }
 }
