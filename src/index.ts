e4da2f99e099c9750522ac6915087c96f43c01a8
Thomas Müller <thomas.mueller@tmit.eu>
2019-09-17 18:26:59 +0200

    storage.constructor.name can be undefined ....

--- src/index.ts
+++ src/index.ts
@@ -104,7 +104,7 @@ export class VuexPersistence<S> implements PersistOptions<S> {
     }
 
     this.asyncStorage = options.asyncStorage || false
-    const storageConstructor = this.storage && this.storage.constructor && this.storage.constructor.name.toLowerCase()
+    const storageConstructor = this.storage && this.storage.constructor && this.storage.constructor.name && this.storage.constructor.name.toLowerCase()
     this.asyncStorage = this.asyncStorage || storageConstructor === 'localforage'
 
     if (this.asyncStorage) {
@@ -142,7 +142,7 @@ export class VuexPersistence<S> implements PersistOptions<S> {
           : ((key: string, state: {}, storage: AsyncStorage) =>
               (storage).setItem(
                 key, // Second argument is state _object_ if localforage, stringified otherwise
-                (((storage && storage.constructor && storage.constructor.name.toLowerCase()) === 'localforage')
+                (((storage && storage.constructor && storage.constructor.name && storage.constructor.name.toLowerCase()) === 'localforage')
                     ? merge({}, state || {})
                     : (
                       this.supportCircular
